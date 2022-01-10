ce47eaefaff47e65f582582242208c7085bb1c7d
Kaia <96497010+KaiaX926@users.noreply.github.com>
2022-01-10 10:43:46 -0500

    Update 046 Permutations.py

--- 046 Permutations.py
+++ 046 Permutations.py
@@ -1,4 +1,12 @@
-# The core idea is to use dynamic programming by changing the position of each pairs of numbers in order.
+# The core idea is to use dynamic programming (DFS) by changing the position of each pair of numbers in order. [DFS method goes as far as possible before backtracking]
+# The first function is used to output the final result, and the second function is the main part of obtaining potential permutation.
+# The thinking flow is that we try to change the order of the last several numbers. Treat the last several numbers as a new subset and recursively start the permutation step inside the new subset.
+# For example, in the first step, we want to get a new list by changing the order of the last number. Of course, there is only one solution, so we add it directly to the output list.
+# Then, in the second step, we try to get a new list by changing the order of the last two numbers. So we treat the last two numbers as a new set and try to find the permutations in the new set by using the same function, which will start from the last number in the new set again. Then add all the solutions to the final list.
+# Recursively find the solution by adding one more number from the righthand until we have considered all positions in the list. 
+# While the overall process starts from the righthand, the certain permutation method in each step begins from the lefthand by exchanging the number on the current place with each number after it.
+# For example, in the third step, we want to change the order of the last three numbers. So we first treat it as a new subset.
+# Then we first exchange the numbers on the first and second places in the set, then the first and third places.
 
 class Solution(object):
     def permute(self, nums):
