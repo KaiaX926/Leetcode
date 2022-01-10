62b40a72c83baf4aad6d89bd7241d3ba7a1de248
Kaia <96497010+KaiaX926@users.noreply.github.com>
2022-01-10 10:12:50 -0500

    Create 046 Permutations.py

---
+++ 046 Permutations.py
@@ -0,0 +1,20 @@
+# The core idea is to use dynamic programming by changing the position of each pairs of numbers in order.
+
+class Solution(object):
+    def permute(self, nums):
+        # DPS with swapping
+        res = []
+        if len(nums) == 0:
+            return res
+        self.get_permute(res, nums, 0)
+        return res
+
+    def get_permute(self, res, nums, index):
+        if index == len(nums):
+            res.append(list(nums))
+            return
+        for i in range(index, len(nums)):
+            nums[i], nums[index] = nums[index], nums[i]
+            # s(n) = 1 + s(n-1)
+            self.get_permute(res, nums, index + 1)
+            nums[i], nums[index] = nums[index], nums[i]
