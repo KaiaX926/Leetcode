44bf2e2fde3388db2afd353f2029087ceee1ced3
Kaia <96497010+KaiaX926@users.noreply.github.com>
2022-01-11 16:23:29 -0500

    Create 056 Merge Intervals.py

---
+++ 056 Merge Intervals.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def merge(self,intervals):
+        """
+        :type intervals: List[List[int]]
+        :rtype: List[List[int]]
+        """
+        intervals.sort()
+        
+        if len(intervals) <= 1:
+            return intervals
+        res = []
+        i = 1
+        
+        while i < len(intervals):
+            #print(intervals)
+            if intervals[i][0] <= intervals[i-1][1]:
+                x1 = min(intervals[i][0],intervals[i-1][0])
+                x2 = max(intervals[i][1],intervals[i-1][1])
+                x = [[x1,x2]]
+                intervals = intervals[:i-1] + x + intervals[i+1:]
+                
+            else:
+                i += 1
+        return intervals
