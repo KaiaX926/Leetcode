794958c673790fa6bcf7b14b48dbdb27234a0c1f
Kaia <96497010+KaiaX926@users.noreply.github.com>
2022-01-25 11:17:24 -0500

    Create 0#93 Restore IP Addresses.py

---
+++ 0#93 Restore IP Addresses.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def restoreIpAddresses(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        
+        res = []
+        i,t,r = 0,0,s
+        
+        if len(s) > 12:
+            return res
+        
+        step = 1 #len(s)//4
+
+        def fetch(s,i,res,r,t,step):
+            l = step
+            while i <= len(r) and t <= 3 and l <= min(3,len(s) - i + 1):
+                if ((int(r[i:i+l]) == 0 and l == 1 ) or (r[i] != '0' and int(r[i:i+l]) > 0))and int(r[i:i+l]) <= 255:
+                    r = r[:i+l] + '.' + r[i+l:]
+                    t += 1
+                    i += l+1 # i = i + l + 1
+                    fetch(s,i,res,r,t,step)
+                    i -= l+1
+                    r = r[:i+l] + r[i+l+1:] #r = r[:i+l] + '.' + r[i+l:]
+                    
+                    t -= 1
+                    l += 1
+                else:
+                    break 
+                      
+            if t == 3 and ((int(r[i:]) == 0 and len(r[i:]) == 1 )or (r[i] != '0' and int(r[i:]) > 0))and int(r[i:]) <= 255:
+                if r not in res:
+                    res.append(r)
+            
+            return
+       
+        fetch(s,i,res,r,t,step)
+        return res
