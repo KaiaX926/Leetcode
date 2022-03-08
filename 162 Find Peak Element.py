40c746e722ab47a6f7f3fd8481122cd0ed42bc4b
Kaia <96497010+KaiaX926@users.noreply.github.com>
2022-03-08 10:20:26 -0500

    Create 162 Find Peak Element.py

---
+++ 162 Find Peak Element.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findPeakElement(self, nums: List[int]) -> int:
+        nums = [-float('inf')] + nums + [-float('inf')]
+        for i in range(1,len(nums)):
+            if nums[i] > nums[i-1] and nums[i] > nums[i+1]:
+                return i-1
